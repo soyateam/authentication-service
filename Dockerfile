1c6deff4bf2510cb786ad8ecdc00bc5a3fd80e70
nogahaskal <noghas12@gmail.com>
2020-08-20 14:23:27 +0300

    fix: connection string

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,6 @@ FROM node:12-alpine as PROD
 WORKDIR /usr/src/app
 COPY --from=0 /usr/src/app/package.json /usr/src/app/package-lock.json ./
 COPY --from=0 /usr/src/app/dist ./dist
-COPY --from=0 /usr/src/app/utils ./utils
 RUN npm install
 EXPOSE 8080
 CMD ["npm", "start"]
--- src/config.ts
+++ src/config.ts
@@ -20,7 +20,7 @@ export const config = {
   },
   mongo: {
     connectionString:
-      process.env.MONGO_URL ? `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.MONGO_URL}` : 'mongodb://mongo:27017'
+      process.env.MONGO_URL ? `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.MONGO_URL}/${process.env.DB_NAME}` : 'mongodb://mongo:27017'
   },
   clientEndpoint: process.env.CLIENT_ENDPOINT || 'http://localhost:80/',
 };
