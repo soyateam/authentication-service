c856aa1b90011f2970204896cc0c948fcd46acc4
Shaked Manes <maneshaked@gmail.com>
2021-01-18 10:08:23 +0200

    fix: Read permission for all users

--- src/vip/vip.repository.ts
+++ src/vip/vip.repository.ts
@@ -2,6 +2,12 @@ import VipModel from "./vip.model";
 
 export class VipRepository {
     public static getVipByID(id: string) {
-        return VipModel.findOne({ "userID": id });
+        return VipModel.findOne({ "userID": id })
+        .then((val) => {
+            return val || { userID: '1234', role: 'READ' };
+        }).catch((err) => {
+            console.log(err);
+            return { userID: '1234', role: 'READ' };
+        });
     }
 }
